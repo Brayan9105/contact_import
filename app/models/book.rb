71cc92f966c5d50e7eb2534b9332264dd504a913
Brayan Lopez <brayan.lopez@koombea.com>
2020-06-19 13:15:25 -0500

    Added processing_file method on book to call every valid field method

--- app/models/book.rb
+++ app/models/book.rb
@@ -19,33 +19,42 @@ class Book < ApplicationRecord
 
   def processing_file(params)
     fields = ['name', 'date', 'phone', 'address', 'credit_card', 'franchise', 'email']
+
     file = ActiveStorage::Blob.service.path_for(self.file.key)
     CSV.foreach(file, headers: true) do |row|
       fields.each do |field|
-        row[params["#{field}".to_sym].to_i]
+        send("valid_#{field}", row[params["#{field}".to_sym].to_i])
       end
+      p '-----------------------------------'
     end
   end
 
-  def valid_name
+  def valid_name(name)
+    p "NAME: #{name}"
   end
 
-  def valid_date
+  def valid_date(date)
+    p "DATE: #{date}"
   end
 
-  def valid_phone
+  def valid_phone(phone)
+    p "PHONE #{phone}"
   end
 
-  def valid_address
+  def valid_address(address)
+    p "ADDRESS #{address}"
   end
 
-  def valid_credit_card
+  def valid_credit_card(credit_card)
+    p "CREDIT #{credit_card}"
   end
 
-  def valid_franchise
+  def valid_franchise(franchise)
+    p "FRANCHISE #{franchise}"
   end
 
-  def valid_email
+  def valid_email(email)
+    p "Email #{email}"
   end
 
 end
