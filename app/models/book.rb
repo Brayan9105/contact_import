c623f25e607afa66fbe2e05721ba5c3907977d4b
Brayan Lopez <brayan.lopez@koombea.com>
2020-06-19 22:59:08 -0500

    Added validation name, date, phone and address

--- app/models/book.rb
+++ app/models/book.rb
@@ -18,38 +18,57 @@ class Book < ApplicationRecord
   end
 
   def processing_file(params)
-    fields = ['name', 'date', 'phone', 'address', 'credit_card', 'franchise', 'email']
+    # fields = ['name', 'date']#, 'phone', 'address', 'credit_card', 'franchise', 'email']
+    # valid = true
+    # valid_contact = 0
 
-    file = ActiveStorage::Blob.service.path_for(self.file.key)
-    CSV.foreach(file, headers: true) do |row|
-      fields.each do |field|
-        send("valid_#{field}", row[params["#{field}".to_sym].to_i])
-      end
-      p '-----------------------------------'
-    end
+    # file = ActiveStorage::Blob.service.path_for(self.file.key)
+    # CSV.foreach(file, headers: true) do |row|
+    #   fields.each do |field|
+    #     valid &&= send("valid_#{field}", row[params["#{field}".to_sym].to_i])
+
+    #     unless valid
+    #       #call a method to create a invalid contact with error attached
+    #       break
+    #     end
+    #   end
+
+    #   valid_contact += 1 if valid
+    #   p '-----------------------------------'
+    # end
+    # self.terminado! if valid_contact > 0
+    
   end
 
   def valid_name(name)
-    p "NAME: #{name}"
+    name.match(/[!$%^&*()_+|~=`{}\[\]:";'<>?,.Ç¨\|@#¢∞¬÷“”≠´\\]/) ? false : true
   end
 
   def valid_date(date)
-    p "DATE: #{date}"
+    begin
+      # (Date.parse str).strftime("%Y %B %d")
+      Date.iso8601(date)
+      true 
+    rescue
+      false
+    end
   end
 
   def valid_phone(phone)
-    p "PHONE #{phone}"
+    regex1 = /^[\(]\+[\d]{2}[\)][ ][\d]{3}[ ][\d]{3}[ ][\d]{2}[ ][\d]{2}$/
+    regex2 = /^[\(]\+[\d]{2}[\)][ ][\d]{3}[-][\d]{3}[-][\d]{2}[-][\d]{2}$/
+    phone.match(regex1) || phone.match(regex2) ? true : false
   end
 
   def valid_address(address)
-    p "ADDRESS #{address}"
+    address.present?
   end
 
   def valid_credit_card(credit_card)
     p "CREDIT #{credit_card}"
   end
 
-  def valid_franchise(franchise)
+  def valid_franchise(franchise)ex
     p "FRANCHISE #{franchise}"
   end
 
