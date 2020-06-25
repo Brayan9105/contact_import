368b872893e291256ee1b99d55a1b6dcd20ed2d6
Brayan Lopez <brayan.lopez@koombea.com>
2020-06-24 19:56:41 -0500

    Added books show feature test

--- spec/features/books/show_spec.rb
+++ spec/features/books/show_spec.rb
@@ -1,36 +1,32 @@
 require 'rails_helper'
 
-RSpec.describe 'Books index', type: :feature do
+RSpec.describe 'Books show', type: :feature do
   before(:each) do
     @user = User.create(password: '123456', username: 'testing', email: 'testing@mail.com')
     @book = Book.create(file: fixture_file_upload(Rails.root.join("spec", "fixtures", "contacts.csv")), user_id: @user.id)
   end
 
-  describe 'book index' do
+  describe 'book show' do
 
     before(:each) do
       visit '/users/sign_in'
       fill_in('Email', with: 'testing@mail.com')
       fill_in('Password', with: '123456')
       click_button('Log in')
-      visit '/books'
+      visit '/books/1'
     end
 
-    scenario 'visit books new' do
-      expect(page).to have_http_status(200)
-      expect(page).to have_content('My books')
-    end
-
-    scenario 'click on import new book' do
-      click_on 'Import new book'
-      expect(page).to have_http_status(200)
-      expect(page).to have_content('New book')
-    end
-
-    scenario 'click on view' do
-      click_on 'view'
+    scenario 'visit books show' do
       expect(page).to have_http_status(200)
       expect(page).to have_content('contacts.csv')
+      expect(page).to have_content('espera')
+      expect(page).to have_content('Name')
+      expect(page).to have_content('Date')
+      expect(page).to have_content('Phone')
+      expect(page).to have_content('Address')
+      expect(page).to have_content('Franchise')
+      expect(page).to have_content('Email')
+      expect(page).to have_content('Processing')
     end
   end
 end
